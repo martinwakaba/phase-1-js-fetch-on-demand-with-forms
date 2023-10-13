8620ef38a94ce42d5b32c38e408cfb5c5d766ba8
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-10-13 20:41:00 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputform = document.querySelector('form');
+  inputform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#searchByID');
+    console.log(input.value);
+
+   fetch(`http://localhost:3000/movies/${input.value}`)
+   .then(response => response.json())
+   .then(data => {
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+
+    
+   })
+   
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
